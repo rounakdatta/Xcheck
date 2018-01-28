2502fc34d19fbe9d82ba2fd8665b3fcd2e30c505
rounakdatta12@gmail.com <rounakdatta12@gmail.com>
2018-01-28 08:55:36 +0530

    rm -rf

--- app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FakenewsfeConfig(AppConfig):
-    name = 'fakenewsFE'
--- app/tocsv.py
+++
@@ -1,7 +0,0 @@
-def text2csv(screenname):
-    t=""
-    data_file = open("fakenewsFE\data.csv", 'w')
-    for s in screenname:
-       if(s!=',' and s!='\n'):
-           t=t+s
-    data_file.write('text, \n'+t+',')
